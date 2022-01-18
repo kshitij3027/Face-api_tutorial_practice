787e3b2352fcc63071c4b6c93d8e782a52bc2a0a
Kshitij Kakade <54987874+kshitij3027@users.noreply.github.com>
2022-01-18 05:54:36 -0800

    Added all the project files

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../App.css';
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <span>KsK</span>
+        </div>
+    )
+}
+
+export default Navbar
